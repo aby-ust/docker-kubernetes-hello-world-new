f06c9c81eacd744b7d8db338145ea165c65cb6f8
ksreelakshmi0508 <110027141+ksreelakshmi0508@users.noreply.github.com>
2022-08-26 00:12:39 +0530

    Update HelloController.java

--- src/main/java/org/examples/java/helloworld/HelloController.java
+++ src/main/java/org/examples/java/helloworld/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello world + Spring Boot!";
+        return "Hello world !";
     }
     
 }
